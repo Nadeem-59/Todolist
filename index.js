72867d4d4b3afd8826cffdb297fde039e24980e1
Mustabshira khalid <160775326+Nadeem-59@users.noreply.github.com>
2024-04-16 01:52:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todolist = [];
+let conditions = true;
+console.log(chalk.red.bold("\n \welcome to my todolist\n "));
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.yellow("Enter your new task"),
+        }
+    ]);
+    todolist.push(addTask.task);
+    console.log(chalk.green(`${addTask.task}task add in Todo_list successfully `));
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more task ",
+            default: "False",
+        }
+    ]);
+    conditions = addMoreTask.addMore;
+}
+console.log(chalk.blue("your update Todo-list", todolist));
